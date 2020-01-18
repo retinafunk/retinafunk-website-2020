dd423ad74e00c7462324a860ebb4c1a64571e5db
Andy Weisner <retionafunk@gmail.com>
2020-01-18 23:13:57 +0100

    test: test CSS integration

--- processwire/site/templates/frontend/www/src/js/lit-elem-demo.js
+++ processwire/site/templates/frontend/www/src/js/lit-elem-demo.js
@@ -2,7 +2,6 @@ import {LitElement,html} from "lit-element";
 
 class LitElemDemo extends LitElement{
     render() {
-      //  super.render();
         return html`
             <p style="color:red;">YAY an Lit Element !Y!</p>
         `;
