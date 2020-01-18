dd49dceda154d41399cae23fbf649c0fb650ae62
Andy Weisner <retionafunk@gmail.com>
2020-01-18 18:47:32 +0100

    test: include and test js scripts in PW template

--- processwire/site/templates/basic-page.php
+++ processwire/site/templates/basic-page.php
@@ -3,11 +3,18 @@
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
     <title><?php echo $page->title; ?></title>
-    <link rel="stylesheet" type="text/css" href="<?php echo $config->urls->templates ?>styles/main.css"/>
+    <link rel="stylesheet" type="text/css" href="<?=$config->urls->templates;?>styles/main.css"/>
+    <script defer type="module" src="<?=$config->urls->templates;?>frontend/www/lib/js/app.js"></script>
 </head>
 <body>
 <h1><?php echo $page->title; ?></h1>
 <?php if ($page->editable()) echo "<p><a href='$page->editURL'>Edit</a></p>"; ?>
+<div>
+    <div id="app"></div>
+    <div id="app2"></div>
+    <div id="gql-result"></div>
+    <lit-elem-demo></lit-elem-demo>
+</div>
 
 </body>
 </html>
