e03c4419461c4d66a155834e228f44984d275630
Andy Weisner <retionafunk@gmail.com>
2020-02-16 20:17:21 +0100

    refactor basicPage pw template to use regions

--- processwire/site/templates/basic-page.php
+++ processwire/site/templates/basic-page.php
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-    <title><?php echo $page->title; ?></title>
-    <link rel="stylesheet" type="text/css" href="<?=$config->urls->templates;?>frontend/www/lib/styles/index.css"/>
-    <script defer type="module" src="<?=$config->urls->templates;?>frontend/www/lib/js/app.js"></script>
-</head>
-<body>
-<h1><?php echo $page->title; ?></h1>
-<?php if ($page->editable()) echo "<p><a href='$page->editURL'>Edit</a></p>"; ?>
-<div>
-    <div id="app" class="bg-blue-100 text-xl"></div>
-    <div id="app2"></div>
-    <div id="gql-result"></div>
-    <lit-elem-demo></lit-elem-demo>
-</div>
+<?php
 
-</body>
-</html>
+?>
+<main pw-replace="content">
+    <article class="">
+        <?=$page->body;?>
+    </article>
+</main>
