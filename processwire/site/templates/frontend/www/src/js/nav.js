377a2852eb52173c20a3fd0d8100d527a3d707ab
Andy Weisner <retionafunk@gmail.com>
2020-02-16 22:31:42 +0100

    refactor and improve nav behaviour

--- processwire/site/templates/frontend/www/src/js/nav.js
+++ processwire/site/templates/frontend/www/src/js/nav.js
@@ -1,11 +1,11 @@
-export default function(){
-    console.log('nav module!');
+export default function () {
+    //console.log('nav module');
     const navTrigger = document.querySelector('#nav-trigger');
-    if(navTrigger){
-        navTrigger.addEventListener('click',evt=>{
+    if (navTrigger) {
+        navTrigger.addEventListener('click', evt => {
             evt.preventDefault();
-            const navPanel = document.querySelector('.primary-nav');
-            if(navPanel) navPanel.classList.toggle('is-open');
+            const outerContainer = document.querySelector('body');
+            if (outerContainer) outerContainer.classList.toggle('is-open');
             console.log('open');
         });
     }
