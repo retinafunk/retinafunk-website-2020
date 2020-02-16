8716abf548350521a7c74df23cece097ff0a8af6
Andy Weisner <retionafunk@gmail.com>
2020-02-16 20:20:25 +0100

    enable pjax AJAX navigation with use pjax lib , import nav module

--- processwire/site/templates/frontend/www/src/js/app.js
+++ processwire/site/templates/frontend/www/src/js/app.js
@@ -1,25 +1,38 @@
 import {html,render} from "lit-html";
 import {until} from "lit-html/directives/until.js";
 import {litElem} from "./lit-elem-demo.js";
+import Pjax from 'pjax';
+import Nav from './nav.js';
 
-console.log('app.js loaded!!!X');
+
+const pjax = new Pjax({
+    selectors: ['#content']
+});
+document.addEventListener('pjax:complete',evt=>{
+    const navPanel = document.querySelector('.primary-nav');
+    if(navPanel && navPanel.classList.contains('is-open')) navPanel.classList.remove('is-open') ;
+});
+
+const nav = Nav();
 
 const rfTemplate = (name,classParam) => html`<p class=${classParam}>Hi ${name} </p><button @click=${clickHandler}>YAY</button>`;
 
 const conditionalTemplate= ( user) => html`
     ${user.isVisible
-        ? html`Welcome ${user.name}`
+        ? html`Welcome ${user.name}` 
         : html` Nada `
     }
 `;
 
 const pwDataTemplate=(data)=> html`
-    <ul>
-        <li> <label for="">Title:</label> ${data.home.first.title}</li>
-        <li> <label for="">Title:</label> ${data.home.first.url}</li>
-        <li> <label for="">Title:</label> ${data.home.first.email}</li>
-        <li> <label for="">Title:</label> ${data.home.first.name}</li>
-    </ul>
+    <div>
+        <div>
+             <h2>Title: ${data.basicPage.first.title}</h2> 
+             <div>
+                ${data.basicPage.first.body} 
+            </div>
+        </div>
+    </div>
 `;
 
 const clickHandler = () =>{
@@ -35,13 +48,13 @@ const fetchData = () =>{
         },
         body: JSON.stringify({
             query: `
-                    {
-                      home {
-                        first {
-                          url,name,email,title,headline 
-                        }
-                      }
+               {
+                  basicPage(s:"title*=intro") {
+                    first { 
+                      title,body
                     }
+                  }
+               }
  `,
         }),
     })
@@ -50,13 +63,17 @@ const fetchData = () =>{
         })
         .then(responseAsJson => {
             console.log('response',responseAsJson.data);
-            render(pwDataTemplate(responseAsJson.data),document.querySelector('#gql-result'));
+            if(responseAsJson.data && document.querySelector('#gql-result')){
+                render(pwDataTemplate(responseAsJson.data),document.querySelector('#gql-result'));
+            }
+
             //this.setState({ loading: false, data: responseAsJson.data })
         })
 };
 
 const hideMe = false;
-
+fetchData();
+/*
 render(rfTemplate('Andy','is-bold'),document.querySelector('#app'));
 
-render(conditionalTemplate({name: 'Andy',isVisible:true}),document.querySelector('#app2'));
+render(conditionalTemplate({name: 'Andy',isVisible:true}),document.querySelector('#app2'));*/
